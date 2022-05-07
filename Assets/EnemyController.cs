417c5a90fcb76b9ee00b34ec917e34c8a3c8073d
Luan de Paula Mota <84395984+yuhohah@users.noreply.github.com>
2022-05-07 05:01:10 -0300

    Alteracao 1
    Criação de esquema de primeira pessoa, controles de movimento básico e gravidade do personagem.

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    //public Player_Controller pc;
+    public Rigidbody rb;
+    public int velocidadeDeMovimento;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rg.addForce(0, velocidadeDeMovimento, 0);
+        
+    }
+}
